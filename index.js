6576f94db64ec28960729259a2d3bb6c3c6411a9
Lesaloon <guilliancarville@gmail.com>
2021-07-16 11:40:41 +0200

    idk

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { Client, MessageEmbed } = require('discord.js');
 const client = new Client();
 const Filter = require('bad-words'),
 filter = new Filter();
+const util = require("./util");
 
 client.login(process.env.TOKEN);
 
@@ -13,11 +14,27 @@ client.on('message', async message => {
 	// Voice only works in guilds, if the message does not come from a guild,
 	// we ignore it
 	if (!message.guild) return;
-
+	if (message.author.id == client.user.id ) return;
 
 	if (message.channel.name == "gpt-question-proposition") {
+		
+
+
 		if (message.content.length <= 30) {
-			console.log("New proposition :", filter.clean(message.cleanContent))
+			const embed = new MessageEmbed()
+      		// Set the title of the field
+			.setTitle('New Proposition')
+			// Set the color of the embed
+			.setColor('#57F287')
+			// Set the main content of the embed
+			.setDescription(`A new Question from: ${message.author.username}#${message.author.discriminator}` )
+
+			.setFooter("Bot made by LESALOON#1956. Powered by Open IA GPT-3")
+
+			.addField("The Question:", filter.clean(message.content));
+			
+			message.channel.send(embed);
+
 		} else {
 			const embed = new MessageEmbed()
       		// Set the title of the field
@@ -27,7 +44,11 @@ client.on('message', async message => {
 			// Set the main content of the embed
 			.setDescription('Question cant be more than 30 character long');
 			// Send the embed to the same channel as the message
-			message.channel.send(embed);
+			try {
+				message.author.dmChannel.send(`<@${message.author.id}>`,embed);
+			} catch (error) {
+				message.channel.send(`<@${message.author.id}>`,embed);				
+			}
 		}
 		
 		
@@ -35,3 +56,20 @@ client.on('message', async message => {
 	}
 	
 });
+
+
+function AskRoutine() {
+	
+	// get the message w/ the most upvote
+	// delet it
+	// ask GTP3
+	// send the message in the gtp3 responce chanel
+}
+
+setInterval(() => {
+	AskRoutine()
+}, 60*100) 
+// one day 24*60*60*100
+// 12 h = 12*60*60*100
+// 6 h = 6*60*60*100
+// 3 h = 3*60*60*100
---
+++ util.js
@@ -0,0 +1,36 @@
+const Emojis = {
+    TIMER: "⏲",
+    PING_PONG: "🏓",
+    DANGER: "❗❗",
+    MUSIC: "🎶",
+    SEARCH: "🔎",
+    SUCCESS: "👍",
+    SAD: "😔",
+    PLAY: "▶️",
+    PAUSE: "⏸️",
+    WAVE: "👋",
+    REPEAT: "🔄",
+    SINGLE_REPEAT: "🔂",
+    CHECK: "✅",
+    CROSS: "❌",
+    NOTEPAD: "🗒"
+};
+
+const Colors = {
+    WHITE: 16777215,
+    BLURPLE: 5793266,
+    GREYPLE: 10070709,
+    DARK_BUT_NOT_BLACK: 2895667,
+    NOT_QUITE_BLACK: 2303786,
+    GREEN: 5763719,
+    YELLOW: 16705372,
+    FUSCHIA: 15418782,
+    RED: 15548997,
+    BLACK: 2303786,
+    
+};
+
+module.exports =  {
+    Emojis: Colors,
+    Colors: Colors
+}
