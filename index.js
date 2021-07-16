8d8d7dac1208216dc95fd02236af1ca4f3bc66b8
Lesaloon <guilliancarville@gmail.com>
2021-07-16 15:30:39 +0200

    gtp 3 is responding

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ client.on('message', async message => {
 			.addField("Inapropriate Question ?", 
 			"If you think the question above is inappropriate (politics / controvertial subject / bad word / nsfw ) or could provoke an inappropriate response please refer immediately to an administrator",
 			true);
-			message.channel.send(embed).react(util.Emojis.CHECK);
+			(await message.channel.send(embed)).react(util.Emojis.CHECK);
 
 		} else {
 			const embed = new MessageEmbed()
@@ -81,36 +81,56 @@ function AskRoutine(guild) {
 	
 	channelP.messages.fetch().then(messages => {
 		const message = getHighestReactionAmount(messages, util.Emojis.CHECK); // get the message w/ the most upvote
-		console.log(message.embeds[0]);
+		//console.log(message.embeds[0]);
 		//message.react("👍");
 		
 		
 
 		// ask GTP3
+		const gptResponse = openai.complete({
+			engine: 'davinci',
+			prompt: 
+`The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
 
-		// send the message in the gtp3 responce chanel
-		const channelA = guild.channels.cache.find( channel => channel.name == "gpt-answer" )
-		
-		const embed = new MessageEmbed()
-		// Set the title of the field
-		.setTitle('Responce time')
-		// Set the color of the embed
-		.setColor(util.Colors.BLURPLE)
-		// Set the main content of the embed
-		.setDescription(`This is the responce of GTP-3 on the question of : ${message.embeds[0].description.split(": ")[1]}`)
-		.addField("Question: ", `${message.embeds[0].fields[0].value}`)
-		.addField("Anser from GTP-3: ", `${GTP3A}`)
-		.setAuthor(client.user.username)
-		.setFooter("Powered by Open IA GPT-3");
+H:${message.embeds[0].fields[0].value}
+A:`,
+			maxTokens: 50,
+			temperature: 0.9,
+			topP: 1,
+			presencePenalty: 0,
+			frequencyPenalty: 0,
+			bestOf: 1,
+			n: 1,
+			stream: false,
+			stop: ['\n', "H:", "A:"]
+		}).then((gptResponse)=> {
+			console.log(gptResponse.data.choices[0].text);
+			// send the message in the gtp3 responce chanel
+			const channelA = guild.channels.cache.find( channel => channel.name == "gpt-answer" )
+			
+			const embed = new MessageEmbed()
+			// Set the title of the field
+			.setTitle('Responce time')
+			// Set the color of the embed
+			.setColor(util.Colors.BLURPLE)
+			// Set the main content of the embed
+			.setDescription(`This is the responce of GTP-3 on the question of : ${message.embeds[0].description.split(": ")[1]}`)
+			.addField("Question: ", `${message.embeds[0].fields[0].value}`)
+			.addField("Answer from GTP-3: ", `${gptResponse.data.choices[0].text}`)
+			.addField("Shoking answer ?", "If you think the answer above is inappropriate (politics / controvertial subject / bad word / nsfw ) please refer immediately to an administrator and take note that GPT-3 has no filter and has learned from internet. We do not control his answer in any way")
+			.setAuthor(client.user.username)
+			.setFooter("Powered by Open IA GPT-3");
+	
+			channelA.send(embed);
 
-		channelA.send(embed);
+			// delete all the msg in the chanel
+			messages.map((m) => m.delete() );
 
+		});
 	})
-	
-	
 
 
-	// delete all the msg in the chanel
+	
 }
 
 function getHighestReactionAmount(messages, reaction) {
@@ -125,13 +145,14 @@ function getHighestReactionAmount(messages, reaction) {
 			TopMsg = message;
 			return
 		}
-
+		console.log(TopMsg)
 		if(TopMsg.reactions.cache.first().count < message.reactions.cache.first().count ) { 
 			TopMsg = message;
 			return
 		}
 	
 	});
+	if (TopMsg.reactions.cache.first().count == 1) return null
 	return TopMsg
 }
 
